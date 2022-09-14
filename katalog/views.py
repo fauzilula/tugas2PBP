1514e6e0dd2b3d9d2f0fab64b7c211738a9d2506
fauzilula <fauzil.ula@ui.ac.id>
2022-09-14 15:59:48 +0700

    Tugas 2 PBP done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+     data_katalog = CatalogItem.objects.all()
+     context = {
+         'list_barang': data_katalog,
+         'nama': 'Fauzil Ula Fachrudin',
+         'NPM': '2106702680'   
+     }
+     return render(request, "katalog.html", context)
